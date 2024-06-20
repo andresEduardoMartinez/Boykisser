c5d8d31c92a74b5ca635c8b914df9bb96148d018
Andres Eduardo Martinez <166736006+andresEduardoMartinez@users.noreply.github.com>
2024-06-20 08:58:08 -0300

    Add files via upload

---
+++ src/boykisser/Boykisser.java
@@ -0,0 +1,52 @@
+
+package boykisser;
+
+public class Boykisser {
+    public static void main(String[] args) {
+        System.out.println("         ^.                                                                                ");
+        System.out.println("        lM|_,.                                                                 .I)\\+(~`   ");
+        System.out.println("       .x; `<j1;.                                                           .:)j!.  .\"*`  ");
+        System.out.println("      .u>     .<v+'                        .[{\".                         .:n|,.       _#. ");
+        System.out.println("     .}n.       .\"t(^                      `#^>x>`.                    ^((;'          'ci ");
+        System.out.println("     !u'           ^-n;.                   'c, .\"]n>.                ^)r\"              -z");
+        System.out.println("     x_              '!f_.   .',,:!-_--_-i;:-c`   .>j?.            \"j)`                ,* ");
+        System.out.println("    `c'                ._n)j[_;,```.    .'`,<rz`    .]x`         ^t(`                  '*^ ");
+        System.out.println("    {}                   .:n1'                .       \"c<      ^j(^                     v^");
+        System.out.println("   'c^                     .\"t-'                       ')/`  \"(n`                      v^");
+        System.out.println("   ?r                         ,),                        ,u|^uI                          u^");
+        System.out.println("  .zl                                                     .?W-.                          u^");
+        System.out.println("  ix.                                                      ~l                          'z`");
+        System.out.println("  ~1                                                                                   :# ");
+        System.out.println("  ~1                                                                                  .x{ ");
+        System.out.println("  <)                                                                                  !*' ");
+        System.out.println("  ~)                                                                                 .c;  ");
+        System.out.println("  ~t                                                                                 ~/.  ");
+        System.out.println("  ^&`          ...                                          .''''''.                ^z^   ");
+        System.out.println("  .x}         'vz*ccn1!\"`'                            `:})n*WWWWWWWr;.             'c[   ");
+        System.out.println("   \"v^          .'''\"-fuWWMx{>,`.                 'I(zWMW#cjjr-+~~~<\"             .t|  ");
+        System.out.println("    </.                 '``I+zWWu<               .#&&&W+\".                       ^j>.    ");
+        System.out.println("     ~z`                 `ltM8&x~'                ^+/#&8&*un?~,'.               ;W{,-:,,^ ");
+        System.out.println("      ,f['         '^I{x*&&8v+^     .^><)/\\:'        .`^:{*88&W&##n?,      .,~|(t[-,,``{#");
+        System.out.println(".~!,`. .;-'      ;vW&WMn+<\".        .<i\"\"\"\"\"'              .`i\\*#WWM*!     .^`.      .[n\" ");
+        System.out.println("^M`^;+~~+++\"      \",'.                                           `~,\"^            .\"}v\"   ");
+        System.out.println(".n!                                                             :_~\"!|. ':?I.   l\\(]'     ");
+        System.out.println(" ^c_.       .``'    ..                 .;~\"         1`          .  .}):<],.     ;v+       ");
+        System.out.println("  .{n\"   `!_+,]: `I?_^        `.    .\"\\(l,1j<;`'..`_c^             .i;`.         'xI      ");
+        System.out.println("    '}t}...   l}~[,.         .r\\\"',?f!`    ..\":_xz-,.                             ^c^     ");
+        System.out.println("      !z.      ..              `:;Ir+           \"M`                                iz`    ");
+        System.out.println("     }j'                           `M'          :n                                  c;    ");
+        System.out.println("    !n'                             )n'         ;j                       .?]!.`````IM;    ");
+        System.out.println("   `*`                               /|.        ]x                     '<ci.:il^^^^^.     ");
+        System.out.println("   1\\  .',~_~:.  r(,.                .1|'      .u,                 .`;t},.                ");
+        System.out.println("  'Mt[\\f~l`.'!n{_zM/j-:`'              {x\"     ,u.           .'\":?|ti:'                   ");
+        System.out.println("   ^\".         .^\"tj''\",]f)\".        `t|,\"~//`          1c[I\"''                        ");
+        System.out.println("                   1|.    `!*-`           '```.            :x].                           ");
+        System.out.println("                   .1x\"     ...                              +n;.                         ");
+        System.out.println("                     ,M[                                      .|\\`                        ");
+        System.out.println("                   ^)(!`                                        ;u;                       ");
+        System.out.println("                 '/f`                                            `*~                      ");
+        System.out.println("");
+        System.out.println("I love java <3");
+    }
+    
+}
